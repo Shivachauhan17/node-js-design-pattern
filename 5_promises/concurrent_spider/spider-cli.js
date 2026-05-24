e352d446651a914e362cb244c76ccaf2fe9639e1
Shivachauhan17 <shivanbd2019@gmail.com>
2026-05-24 13:13:04 +0530

    current spider working

--- 5_promises/concurrent_spider/spider-cli.js
+++ 5_promises/concurrent_spider/spider-cli.js
@@ -1,7 +1,7 @@
 import { spider } from "./spider.js";
 const url = process.argv[2];
 const maxDepth = Number.parseInt(process.argv[3], 10) || 1;
-
+console.debug(`calling spider wiht maxDepth: ${maxDepth}`)
 spider(url, maxDepth)
   .then(() => console.log("Downloaded complete"))
   .catch((err) => {
--- 5_promises/concurrent_spider/spider.js
+++ 5_promises/concurrent_spider/spider.js
@@ -1,38 +1,27 @@
+
 import { urlToFileName } from "../../4_async_pattern_with_callbacks/spider/utils.js";
-import { exists, get } from "./utils.js";
+import { exists } from "../promisified_spider/utils.js";
 import { readFile } from "node:fs/promises";
 import { saveFile } from "../../4_async_pattern_with_callbacks/cleaned_spider/spider.js";
 import { getPageLinks } from "../../4_async_pattern_with_callbacks/spider_v2/utils.js";
+import { download } from "../promisified_spider/spider.js";
+
 
 function spiderLinks(currentUrl, body, maxDepth) {
-  
+  console.debug(`spiderLinks(${currentUrl}, ${maxDepth})`)
   if (maxDepth === 1) {
-    return Promise.resolve()
+    return Promise.resolve();
   }
   const links = getPageLinks(currentUrl, body);
-
-  return links.reduce(
-    (prev, link) => prev.then(() => spider(link, maxDepth - 1)),
-    Promise.resolve(),
-  );
+  console.debug(`links:${links.length}`)
+  const promises=links.map(link=>spider(link,maxDepth-1))
+  return Promise.all(promises)
 }
 
-//returning promise chain to the caller
-//this just don't  download the file
-//but also exposes the content of it to the caller
-export function download(url, filename) {
-  console.log(`Downloading ${url} into ${filename}`);
 
-  return get(url).then((content) =>
-    saveFile(filename, content, (err) => {
-      if (err) throw Error("error in saveFile.");
-      return
-    }),
-  );
-}
 
 export function spider(url, maxDepth) {
-  
+  console.debug(`spider(${url}, ${maxDepth})`)
   const filename = urlToFileName(url);
 
   return exists(filename).then((alreadyExists) => {
